c83907a401cd9deb5f02b206688e68cee25adfbf
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-10-03 10:30:44 +0300

    finish add note button sheet

---
+++ lib/Views/Widgets/custom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: const Color(
+          0xff62fcd7,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: const Center(
+        child: Text(
+          "Add",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Views/Widgets/custom_note_button_sheet.dart
+++ lib/Views/Widgets/custom_note_button_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/Views/Widgets/custom_button.dart';
 import 'package:notes_app/Views/Widgets/custom_text_field.dart';
 
 class CustomNoteButtonSheet extends StatelessWidget {
@@ -8,22 +9,31 @@ class CustomNoteButtonSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextFeild(
-            hint: "title",
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextFeild(
-            hint: "content",
-            maxlines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextFeild(
+              hint: "title",
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFeild(
+              hint: "content",
+              maxlines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
