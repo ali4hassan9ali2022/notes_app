cd8349b32ef35b4ca813829025c97525b43bad9f
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-10-03 09:41:58 +0300

    create show modal bottom sheet

--- lib/Views/Widgets/custom_ note_list_view.dart
+++ lib/Views/Widgets/custom_ note_list_view.dart
@@ -6,13 +6,17 @@ class CsutomNoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4),
-          child: CustomNotesItem(),
-        );
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: CustomNotesItem(),
+          );
+        },
+      ),
     );
   }
 }
