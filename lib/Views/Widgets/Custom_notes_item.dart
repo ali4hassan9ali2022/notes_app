6f767a43a8b21319821d436c18e632ae033cd4bd
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-10-04 03:24:55 +0300

    create edit note view

--- lib/Views/Widgets/Custom_notes_item.dart
+++ lib/Views/Widgets/Custom_notes_item.dart
@@ -1,58 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/Views/edit_note_view.dart';
 
 class CustomNotesItem extends StatelessWidget {
   const CustomNotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 24, top: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                "Build your career with Ali Kasrawy",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(bottom: 24, top: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
                 style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black.withOpacity(0.5),
+                  color: Colors.black,
+                  fontSize: 28,
                 ),
               ),
-            ),
-            trailing: const Icon(
-              FontAwesomeIcons.trash,
-              color: Colors.black,
-              size: 24,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "October 3, 2024",
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-                fontSize: 14,
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  "Build your career with Ali Kasrawy",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                "October 3, 2024",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 14,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
